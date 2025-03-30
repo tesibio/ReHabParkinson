484066dc0d56e3b26694e83b89187863caa4b734
cristian <christumer@gmail.com>
2025-03-30 14:48:48 -0600

    Importación de OpenCV como dependencia
    Se usa OpenCV-SDK 4.11.0, estando en el directorio root del proyecto:
    
    ReHabParkinson
      opencv-4.11.0-android-sdk
        OpenCV-android-sdk
    
    Este SDK se puede descargar desde https://github.com/opencv/opencv/releases/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(project(":opencv"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "ReHabParkinson"
 include(":app")
+include(":opencv")
+project(":opencv").projectDir = File(rootDir, "opencv-4.11.0-android-sdk/OpenCV-android-sdk/sdk/")
