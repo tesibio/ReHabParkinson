43727e3658e8c41c717cee0b1b7d0d931448ca0d
Dr.Antonio <labtecorama@gmail.com>
2025-06-04 21:14:08 -0600

    - Corregido conflicto de versiones entre plugins AGP 8.9.1 y 8.4.0 - Integrado módulo OpenCV en proyecto como librería con CMake - Establecida compatibilidad JVM target 17 en todos los módulos - Eliminadas configuraciones gradle desactualizadas y redundantes" - El Apk funciona en emulador

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    plugins {
+        id("com.android.library") version "8.9.1"
+        id("com.android.application") version "8.9.1"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
+
+    }
+
     repositories {
         google {
             content {
@@ -23,4 +30,4 @@ dependencyResolutionManagement {
 rootProject.name = "ReHabParkinson"
 include(":app")
 include(":opencv")
-project(":opencv").projectDir = File(rootDir, "opencv-4.11.0-android-sdk/OpenCV-android-sdk/sdk/")
+project(":opencv").projectDir = File(rootDir, "opencv-4.11.0-android-sdk/OpenCV-android-sdk/sdk")
